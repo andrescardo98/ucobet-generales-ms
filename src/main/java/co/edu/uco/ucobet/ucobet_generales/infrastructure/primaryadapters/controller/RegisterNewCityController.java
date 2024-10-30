d8fceccb14de8f44cc97b2fe3cdcdd50fd3eafd2
Andrés Cardona <andres.cardo98@gmail.com>
2024-10-29 23:05:08 -0500

    Delete RepositoryCustomImpl from infrastructure-secondaryadapter for correct running app

--- src/main/java/co/edu/uco/ucobet/ucobet_generales/infrastructure/primaryadapters/controller/RegisterNewCityController.java
+++ src/main/java/co/edu/uco/ucobet/ucobet_generales/infrastructure/primaryadapters/controller/RegisterNewCityController.java
@@ -8,7 +8,11 @@ import co.edu.uco.ucobet.ucobet_generales.infrastructure.primaryadapters.control
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/general/api/v1/cities")
 public class RegisterNewCityController {
 
     private RegisterNewCityInteractor registerNewCityInteractor;
