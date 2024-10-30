bbd82d497067d7079cd33bb700fdfe3a2fc39e8f
Andrés Cardona <andres.cardo98@gmail.com>
2024-10-29 23:21:38 -0500

    PostMapping and GetMapping annotation for RegisterNewCityController

--- src/main/java/co/edu/uco/ucobet/ucobet_generales/infrastructure/primaryadapters/controller/RegisterNewCityController.java
+++ src/main/java/co/edu/uco/ucobet/ucobet_generales/infrastructure/primaryadapters/controller/RegisterNewCityController.java
@@ -7,9 +7,7 @@ import co.edu.uco.ucobet.ucobet_generales.crosscutting.helpers.UUIDHelper;
 import co.edu.uco.ucobet.ucobet_generales.infrastructure.primaryadapters.controller.response.CityResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/general/api/v1/cities")
@@ -21,6 +19,7 @@ public class RegisterNewCityController {
         this.registerNewCityInteractor = registerNewCityInteractor;
     }
 
+    @PostMapping
     public ResponseEntity<CityResponse> crear(@RequestBody RegisterNewCityDTO dto){
         var httpStatusCode = HttpStatus.CREATED;
         var cityResponse = new CityResponse();
@@ -42,6 +41,7 @@ public class RegisterNewCityController {
         return new ResponseEntity<>(cityResponse, httpStatusCode);
     }
 
+    @GetMapping
     public RegisterNewCityDTO executeDummy(){
         return RegisterNewCityDTO.create("Rionegro", UUIDHelper.generate());
     }
